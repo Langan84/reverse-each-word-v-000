1ac3e8ed1afc8e1e9a4510a341a6585d8510ef25
langan.kg Langan <langan.kg@gmail.com>
2020-06-23 19:52:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  array.collect do {|word| word.reverse}.join(" ")
+sentence.split.collect do {|word| word.reverse}.join(" ")
   
  end
