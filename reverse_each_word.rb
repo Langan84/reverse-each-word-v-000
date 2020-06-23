31b237315e75132c8cf547c21389eb2b760d3e29
langan.kg Langan <langan.kg@gmail.com>
2020-06-23 19:49:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
-  array.collect do |word|
-  word.reverse
+  array.collect do {|word| word.reverse}.join("")
   end
-  array.join(" ")
  end
